235cbee8a0402697e5ecbf0bd201633593381094
wool <lgxwool@163.com>
2019-07-31 11:34:50 +0800

    The project initial ---> first by wool

---
+++ src/main/java/com/wool/community/WoolCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.wool.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoolCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoolCommunityApplication.class, args);
+    }
+
+}
